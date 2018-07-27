b0edb4509b6e31978977671be0b78e5a2aaf228a
Alessandro Costanza <spinore@gmail.com>
2018-07-28 01:38:50 +0200

    Versione funzionante del grabbing dati esterni temperatura

--- readGrabberAndTransmitData.py
+++ readGrabberAndTransmitData.py
@@ -1,8 +1,7 @@
-import requests
 import ConfigParser
-import sys
 import json
 import requests
+import re
 
 config = ConfigParser.RawConfigParser()
 config.read('nasProperties.properties')
@@ -11,32 +10,42 @@ urlAndPort = config.get('domoticz', 'urlPort')
 basePath = config.get('domoticz', 'basePath')
 idxSensor = config.get('domoticz', 'idxSensorExternalTempHum')
 instructionGrabberTempExt = config.get('grabber', 'instructionGrabberTempExt')
-instructionGrabberTempExt = config.get('grabber', 'instructionGrabberHumExt')
+instructionGrabberHumExt = config.get('grabber', 'instructionGrabberHumExt')
 endPointGrabberService = config.get('grabber', 'endPointGrabberService')
 idxSensor = config.get('domoticz', 'idxSensorExternalTempHum')
 comfort = config.get('domoticz', 'comfortHumidityDefaultValue')
 loginCredentialBase64 = config.get('domoticz', 'loginCredentialBase64')
 endpoint = urlAndPort+'/'+basePath
+#regular expressione per machare i numeri con la virgola
+NumberRegex = '[^0-9,]';
 
 #prepare data for call grabber
 response=requests.get(endPointGrabberService,params=instructionGrabberTempExt)
 data = json.loads(response.text)
 temperatureRaw=data[0]["element"]
-print('{0:0.2f};{1:0.2f}%'.format(temperatureRaw, humidityRaw))
-#p = l.index("a")
-#newstr = data[0]["element"].replace("°C", "")
-#print(newstr)
+temperatureRaw = re.sub(NumberRegex,'', temperatureRaw)
+temperatureRaw = temperatureRaw.replace(',','.')
 
-#read data
+response=requests.get(endPointGrabberService,params=instructionGrabberHumExt)
+data = json.loads(response.text)
+humidityRaw=data[0]["element"]
+humidityRaw = re.sub(NumberRegex,'', humidityRaw)
+humidityRaw = humidityRaw.replace(',','.')
 
-#print('{0:0.2f};{1:0.2f}%'.format(temperatureRaw, humidityRaw))
+print(temperatureRaw)
+print(humidityRaw)
+temperatureRawN = float(temperatureRaw)
+humidityRawN = float(humidityRaw)
+
+
+print('{0:0.2f};{1:0.2f}%'.format(temperatureRawN, humidityRawN))
 
 #prepare data
-#dataCluster = str('{0:0.2f}'.format(temperatureRaw))+';'+str('{0:0.2f}%'.format(humidityRaw))+';'+str(comfort)
-#payloadTest = {'type': 'command', 'param': 'udevice', 'nvalue': 0, 'idx':  idxSensor, 'svalue': dataCluster}
-#headers = {'Authorization': 'Basic '+str(loginCredentialBase64)}
+dataCluster = str('{0:0.2f}'.format(temperatureRawN))+';'+str('{0:0.2f}%'.format(humidityRawN))+';'+str(comfort)
+payloadTest = {'type': 'command', 'param': 'udevice', 'nvalue': 0, 'idx':  idxSensor, 'svalue': dataCluster}
+headers = {'Authorization': 'Basic '+str(loginCredentialBase64)}
 
 #call api
-#response=requests.get(endpoint,params=payloadTest,headers=headers)
+response=requests.get(endpoint,params=payloadTest,headers=headers)
 
 
