34197f5c59f79fe219f4496d925259c4f5f400b5
Alessandro Costanza <spinore@gmail.com>
2019-08-07 14:33:12 +0200

    Update readDHT22AndTransmitDataMqtt.py
    modifica per costruzione json to send

--- readDHT22AndTransmitDataMqtt.py
+++ readDHT22AndTransmitDataMqtt.py
@@ -4,7 +4,9 @@ import sys
 import requests
 import Adafruit_DHT
 import paho.mqtt.client as mqtt
+import json
 
+#get constant info
 config = configparser.ConfigParser()
 config.read('nasProperties.properties')
 
@@ -15,18 +17,21 @@ pin = config.get('raspberryPi', 'dht22Pin')
 
 #read data
 humidityRaw, temperatureRaw = Adafruit_DHT.read_retry(22, pin)
+
+#print data for debugging
 print('{0:0.2f};{1:0.2f}%'.format(temperatureRaw, humidityRaw))
 
-#prepare data
+#prepare data to send
 temp = str('{0:0.2f}'.format(temperatureRaw))
 hum = str('{0:0.2f}%'.format(humidityRaw))
-
-payloadTest = str({'temperature': temp, 'humidity':hum})
+data = {"temperature": temp,"humidity": hum}
+# convert into JSON:
+payloadContent = json.dumps(data)
 
 #pushmqt
 client = mqtt.Client()
 client.connect(host, int(port))
-client.publish(topic="TestingTopic", payload=payloadTest, qos=0, retain=False)
+client.publish(topic="TestingTopic", payload=payloadContent, qos=0, retain=False)
 
 
 
